aa45ff1042ba50fca8cecbb2a41acf8bd58d5ed0
Cecilia000 <mayili@kean.edu>
2023-01-18 21:37:38 -0500

    Initial Project Commit

---
+++ CPS2232/src/Area.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Area {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the length of side:");
+		double side = input.nextDouble();
+		double area = Math.sqrt(3)/4*Math.pow(side, 2);
+		double volume = area*side;
+		System.out.println("traingle:"+side);
+		System.out.println("The area is:"+area);
+		System.out.println("The volume of the Triangular prism is:"+volume);
+	}
+
+}
